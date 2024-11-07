3b8dfa05d5dd0307e932b963e3dfb4cbb7a90161
Uriel Rodríguez <162546336+urielrs@users.noreply.github.com>
2024-11-06 22:43:12 -0600

    Create Triangulo.java

---
+++ Triangulo.java
@@ -0,0 +1,17 @@
+
+ class Triangulo implements Figura {
+
+    @Override
+    public void calcularArea() {
+        byte a=8;
+        byte b=5;
+        short c;
+        c = (short)(a * b);
+        c = (short)(c/2);
+        System.out.println("El area del Triangulo es"+c);
+        
+        
+                
+    }
+    
+}
